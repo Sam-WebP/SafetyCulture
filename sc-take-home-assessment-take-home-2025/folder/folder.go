31f186a67726238e2cfe993ebe28e0ada398f579
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-10-03 00:26:50 +1000

    refactor: use pointers for folder maps and add name index

--- sc-take-home-assessment-take-home-2025/folder/folder.go
+++ sc-take-home-assessment-take-home-2025/folder/folder.go
@@ -31,21 +31,27 @@ var (
 )
 
 type driver struct {
-	folderMap      map[string]Folder
-	foldersByOrgID map[uuid.UUID][]Folder
+	folderMap      map[string]*Folder      // key: lower(name)+"_"+orgID.String()
+	foldersByOrgID map[uuid.UUID][]*Folder // key: orgID
+	nameIndex      map[string][]*Folder    // key: lower(name)
 }
 
 func NewDriver(folders []Folder) IDriver {
 	d := &driver{
-		folderMap:      make(map[string]Folder),
-		foldersByOrgID: make(map[uuid.UUID][]Folder),
+		folderMap:      make(map[string]*Folder),
+		foldersByOrgID: make(map[uuid.UUID][]*Folder),
+		nameIndex:      make(map[string][]*Folder),
 	}
 
-	for _, folder := range folders {
+	for i := range folders {
+		folder := &folders[i]
 		key := generateKey(folder.Name, folder.OrgId)
 		d.folderMap[key] = folder
 
 		d.foldersByOrgID[folder.OrgId] = append(d.foldersByOrgID[folder.OrgId], folder)
+
+		lowerName := strings.ToLower(folder.Name)
+		d.nameIndex[lowerName] = append(d.nameIndex[lowerName], folder)
 	}
 
 	return d
